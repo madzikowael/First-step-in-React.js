0fb08b560639ad506ee529184eaeff6a1f164bdc
Magda Lejwoda <le.magda@o2.pl>
2019-10-23 18:51:19 +0200

    Use props and create components Column

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+import Column from "../Column/Column";
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired
+  };
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>
+  };
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} imageUrl={this.props.image}/>
+        <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.columns}>
+          <Column title={'Column 1'}/>
+          <Column title={'Column 2'}/>
+          <Column title={'Column 3'}/>
+        </div>
       </section>
     );
   }
