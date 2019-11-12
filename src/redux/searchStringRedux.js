9b71cd67c206e43af99889dba4de73e9551ae9e3
Magda Lejwoda <le.magda@o2.pl>
2019-11-12 17:44:28 +0100

    Ass Search & search String Redux  add changes to App and store.js

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,4 +1,10 @@
+import searchString from '../components/Search/Search';
+import searchContainer from '../components/Search/SearchContainer';
+
 // selectors
+export const getSearchString = searchString;
+export const countVisibleCards = searchString.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
+export const countAllCards = ({cards}) => cards.length;
 
 // action name creator
 
