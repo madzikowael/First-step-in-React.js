bd52bdd706ac3b7c2090a9f18ae119270ec695c8
Magda Lejwoda <le.magda@o2.pl>
2019-11-21 20:48:31 +0100

    Add component FAQ and add to nav

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import Container from '../Container/Container';
-import Hero from '../Hero/Hero';
-import PropTypes from 'prop-types';
 
-const Info = () => (
+const FAQ = () => (
   <Container>
-    <h2>Info</h2>
+    <h2>FAQ</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    {/* <Hero/>
-     */}
   </Container>
-  
+    
 );
-
-// Info.propTypes = {
-//   titleText: PropTypes.node.isRequired,
-//   image: PropTypes.string,
-// };
-
-
-export default Info;
+  
+export default FAQ;
